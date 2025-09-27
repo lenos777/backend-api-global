0ab8ea72e4e56ff8534112587d3aab4bc6f927e1
lenos777 <petabayttv@gmail.com>
2025-09-27 16:00:09 +0300

    deployment nimadir

--- index.js
+++ index.js
@@ -100,7 +100,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://global-frontend-1.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
