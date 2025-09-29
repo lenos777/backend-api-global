9ad20e3dff3bfee04752ee73212d30a50d0a4561
lenos777 <petabayttv@gmail.com>
2025-09-29 16:58:12 +0300

    retry

--- index.js
+++ index.js
@@ -95,16 +95,7 @@ process.on('SIGINT', async () => {
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: [
-    'http://localhost:5173', 
-    'http://localhost:5174', 
-    'http://localhost:3000',
-    'http://127.0.0.1:5173',
-    'http://127.0.0.1:5174',
-    'http://127.0.0.1:3000',
-    'https://global-frontend-1.onrender.com'
-  ],
-  credentials: true,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
